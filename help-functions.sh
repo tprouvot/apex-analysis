e95cc374f4416a107e679f1d5920f6994ef882ab
Thomas Prouvot <prouvot.t@gmail.com>
2022-10-07 15:42:51 +0200

    Add numberoffiles field

--- help-functions.sh
+++ help-functions.sh
@@ -1,5 +1,60 @@
 #!/bin/sh
 
+export PMD
+export NB_FILES
+export P1
+export P2
+export P3
+export LAST_P1
+export LAST_P2
+export LAST_P3
+
+function export_PMD_variables(){
+	STR=$(<pmd-report2.html)
+
+	PMD=$(awk -F'START_PMD_VERSION|END_PMD_VERSION' '{print $2}' <<< "$STR")
+	NB_FILES=$(awk -F'START_NB_FILES|END_NB_FILES' '{print $2}' <<< "$STR")
+	P1=$(awk -F'START_PRIORITY_1|END_PRIORITY_1' '{print $2}' <<< "$STR")
+	P2=$(awk -F'START_PRIORITY_2|END_PRIORITY_2' '{print $2}' <<< "$STR")
+	P3=$(awk -F'START_PRIORITY_3|END_PRIORITY_3' '{print $2}' <<< "$STR")
+}
+
+function create_code_analysis(){
+	local org_username=$1
+
+	local cmd=$(sfdx force:data:record:create --targetusername $org_username -s CodeAnalysis__c -v "NumberOfFiles__c='$NB_FILES' Version__c='$PMD' Priority1__c='$P1' Priority2__c='$P2' Priority3__c='$P3'" --json --loglevel TRACE)
+	echo $cmd
+	local status=$(jq '.status' <<< $cmd)
+	echo $status
+}
+
+function get_last_code_analysis(){
+	local org_username=$1
+
+	local result=$(sfdx force:data:soql:query --targetusername $org_username --query "SELECT Priority1__c, Priority2__c, Priority3__c FROM CodeAnalysis__c ORDER BY CreatedDate DESC LIMIT 1" --json)
+	local nbResult=$(jq -r ".result.totalSize" <<< $result)
+
+	if [ "$nbResult" = "0" ]
+	then
+		echo "No result found, create new CodeAnalysis__c record"
+		create_code_analysis $org_username
+	else
+		LAST_P1=$(jq -r ".result.records|map(.Priority1__c)|.[]" <<< $result)
+		LAST_P2=$(jq -r ".result.records|map(.Priority2__c)|.[]" <<< $result)
+		LAST_P3=$(jq -r ".result.records|map(.Priority3__c)|.[]" <<< $result)
+
+		if [ $LAST_P1 < $P1 ] || [ $LAST_P2 < $P2 ]
+		then
+			echo "Number of errors increased "
+			exit 1
+		fi
+	fi
+}
+
+#export_PMD_variables
+#create_code_analysis 'generali_SO_shape0410'
+#get_last_code_analysis 'generali_SO_shape0410'
+
 # Replace substring in file
 function replace_in_file(){
 	local searched=$1
@@ -34,13 +89,11 @@ function update_help_menu_with_org_values(){
 	local file="./force-app/main/default/customHelpMenuSections/CustomHelpMenuSection.customHelpMenuSection-meta.xml"
 
 	echo "Replacing values in CustomHelpMenuSection"
-	local orgUrl=$(get_org_url $1)
-	replace_in_file "::ORG_URL::" $orgUrl $file
-
-	local recordId=$(get_record_id $1 "SELECT Id FROM Account WHERE Name='PMDReport' LIMIT 1")
+	local recordId=$(get_record_id $orgAlias "SELECT Id FROM StaticResource WHERE Name='PMDReport' LIMIT 1")
 	replace_in_file "::STATIC_RESOURCE_ID::" $recordId $file
+
+	local orgUrl=$(get_org_url $orgAlias)
+	replace_in_file "::ORG_URL::" $orgUrl $file
 
 	echo "End of replacement"
 }
-
-update_help_menu_with_org_values "test-5bfcvse4x0h5@example.com"
